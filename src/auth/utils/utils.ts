ff03e7f26f998231d7cec2d463d809d77e9e346b
Juan Pablo Navarrete <navarrete0725@gmail.com>
2025-07-25 05:03:53 -0500

    eliminación de archivos innecesarios

--- src/auth/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const JWT_SECRET = process.env.JWT_SECRET || 'supersecretkey'
-
-export const generateToken = (userId: string): string => {
-  return jwt.sign({ userId }, JWT_SECRET, {
-    expiresIn: '7d'
-  })
-}
--- user-service/src/app.ts
+++ user-service/src/app.ts
@@ -1,13 +1,19 @@
-import express from 'express'
+import express, { Application } from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors';
+import morgan from 'morgan';
 import userRoutes from './routes/user.routes'
 import { errorHandler } from './middlewares/errorHandler'
 
 dotenv.config()
 
-const app = express()
+const app: Application = express();
 
-app.use(express.json())
+// Middlewares globales
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Rutas del servicio de usuario
 app.use('/api/users', userRoutes)
