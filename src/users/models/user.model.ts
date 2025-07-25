35a60d74452531c70338cae90977b9d15ebf2b0d
Juan Pablo Navarrete <navarrete0725@gmail.com>
2025-07-25 01:22:27 -0500

    crear modelo de usuarios

---
+++ src/users/models/user.model.ts
@@ -0,0 +1,50 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize  from '../../../config/database';
+
+export class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    full_name: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    birth_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    user_name: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true
+    },
+    password_hash: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    timestamps: false
+  }
+);
